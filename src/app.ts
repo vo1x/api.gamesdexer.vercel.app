a53f71b5fcc3831bdc9a012310ffa29175525414
volx <97158976+vo1x@users.noreply.github.com>
2025-01-11 11:57:37 +0545

    update: add `cors`

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors"; 
 import routes from "./routes";
 
 const app = express();
 
-app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" })); 
 
+app.use(express.json());
 app.use("/", routes);
 
 export default app;
