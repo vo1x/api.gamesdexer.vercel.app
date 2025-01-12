936ce2bac3d035ee88e99eeddb26b539642ee778
volx <97158976+vo1x@users.noreply.github.com>
2025-01-12 13:23:50 +0545

    fix: missing deps

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
 import express from "express";
-import cors from "cors"; 
+import cors from "cors";
 import routes from "./routes";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" })); 
+const allowedOrigins = [
+  "https://apidexer.vercel.app",
+  "https://gamesdexer.netlify.app",
+  "http://localhost:5173",
+];
+
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/", routes);
